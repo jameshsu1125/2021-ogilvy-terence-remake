3b502aab647d48afc0bbfbafb31074a39c0e05bb
James Hsu <70932507+jameshsu1125@users.noreply.github.com>
2021-04-29 12:17:54 +0800

    Initial commit

---
+++ src/Index/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './main.less';
+
+export default class Index extends React.Component {
+	// eslint-disable-next-line no-useless-constructor
+	constructor(props) {
+		super(props);
+		// script
+	}
+
+	componentDidMount() {
+		// script
+	}
+
+	componentDidUpdate() {
+		// script
+	}
+
+	componentWillUnmount() {
+		// script
+	}
+
+	render() {
+		return <div id='Index' />;
+	}
+}
---
+++ template/template.meta.js
@@ -0,0 +1,5 @@
+module.exports = {
+	title: '--- title ---',
+	description: '--- description ---',
+	url: 'https://jameshsu1125.github.io/',
+};

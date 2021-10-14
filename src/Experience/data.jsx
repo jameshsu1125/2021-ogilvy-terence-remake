11d3d8be8c8f713260071e80adecfc3449066058
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-10-14 11:38:46 +0800

    add content of experience

---
+++ src/Experience/data.jsx
@@ -0,0 +1,232 @@
+export const DefaultInternshipExperience = {
+	'1st': [
+		{
+			name: { cht: '詹曜維', eng: 'Tony Chan' },
+			html: '',
+		},
+		{
+			name: { cht: '周于鈴', eng: 'Kilin Chou' },
+			html: '',
+		},
+		{
+			name: { cht: '王俊凱', eng: 'Kai Wang' },
+			html: '',
+		},
+	],
+	'2nd': [
+		{
+			name: { cht: '蔡承翰', eng: 'York Tsai' },
+			html: '',
+		},
+		{
+			name: { cht: '王彤', eng: 'Maxine Wang' },
+			html: '',
+		},
+		{
+			name: { cht: '陳昱安', eng: 'Ellie Chen' },
+			html: '',
+		},
+		{
+			name: { cht: '曾韻丞', eng: 'Andy Tseng' },
+			html: '',
+		},
+		{
+			name: { cht: '張競', eng: 'Jing Zhang' },
+			html: '',
+		},
+		{
+			name: { cht: '葉致綱', eng: 'Pablo Yeh' },
+			html: '',
+		},
+	],
+	'3rd': [
+		{
+			name: { cht: '邱彥蓁', eng: 'Krista Chiu' },
+			html: '',
+		},
+		{
+			name: { cht: '鄒玉珊', eng: 'Albee Tsao' },
+			html: '',
+		},
+		{
+			name: { cht: '盧亭軒', eng: 'Iris Lu' },
+			html: '',
+		},
+		{
+			name: { cht: '蔡育旻', eng: 'Yumin Tsai' },
+			html: '',
+		},
+	],
+
+	'4th': [
+		{
+			name: { cht: '溫婷瑩', eng: 'Tiff Wen' },
+			html: '',
+		},
+		{
+			name: { cht: '唐偉峻', eng: 'Willis Tang' },
+			html: '',
+		},
+		{
+			name: { cht: '曾繁皓', eng: 'Fanhou Zeng' },
+			html: '',
+		},
+		{
+			name: { cht: '范君達', eng: 'Jimmy Fan' },
+			html: '',
+		},
+		{
+			name: { cht: '莊智淵', eng: 'Harper Zhuang' },
+			html: '',
+		},
+	],
+	'5th': [
+		{
+			name: { cht: '錢雅莛', eng: 'Ria Chien' },
+			html: '',
+		},
+		{
+			name: { cht: '江昀潔', eng: 'Yvonne Chiang' },
+			html: '',
+		},
+		{
+			name: { cht: '白 傑', eng: 'Jack Pai' },
+			html: '',
+		},
+	],
+	'6th': [
+		{
+			name: { cht: '錢雅莛', eng: 'Ria Chien' },
+			html: '',
+		},
+		{
+			name: { cht: '江昀潔', eng: 'Yvonne Chiang' },
+			html: '',
+		},
+		{
+			name: { cht: '白 傑', eng: 'Jack Pai' },
+			html: '',
+		},
+	],
+	'7th': [
+		{
+			name: { cht: '何伯濤', eng: 'Casper Ho' },
+			html: '',
+		},
+		{
+			name: { cht: '林叡志', eng: 'Danny Lin' },
+			html: '',
+		},
+		{
+			name: { cht: '施又寧', eng: 'Akimo Shih' },
+			html: '',
+		},
+		{
+			name: { cht: '林峻毅', eng: 'Alan Lin' },
+			html: '',
+		},
+	],
+	'8th ': [
+		{
+			name: { cht: '林亞駿', eng: 'Will Lin' },
+			html: '',
+		},
+		{
+			name: { cht: '蘇怡心', eng: 'Eunice Su' },
+			html: '',
+		},
+	],
+	'9th ': [
+		{
+			name: { cht: '王薇涵', eng: 'Lotus Wang' },
+			html: '',
+		},
+		{
+			name: { cht: '嚴德鈞', eng: 'Tc Yen' },
+			html: '',
+		},
+		{
+			name: { cht: '張容瑄', eng: 'Tina Chang' },
+			html: '',
+		},
+		{
+			name: { cht: '彭亭翊', eng: 'Tina Peng' },
+			html: '',
+		},
+	],
+	'10th ': [
+		{
+			name: { cht: '李承恩', eng: 'Ben Lee' },
+			html: '',
+		},
+		{
+			name: { cht: '林佑珊', eng: 'Brook Lin' },
+			html: '',
+		},
+	],
+	'11th ': [
+		{
+			name: { cht: '林堉涵', eng: 'Pi Lin' },
+			html: '',
+		},
+		{
+			name: { cht: '劉亭妤', eng: 'Amy Liu' },
+			html: '',
+		},
+		{
+			name: { cht: '方瑋婕', eng: 'Erin Fang' },
+			html: '',
+		},
+		{
+			name: { cht: '盛以和', eng: 'Finnick Sheng' },
+			html: '',
+		},
+		{
+			name: { cht: '方聖叡', eng: 'FangFang Fang' },
+			html: '',
+		},
+		{
+			name: { cht: '徐聖勛', eng: 'Andy Hsu' },
+			html: '',
+		},
+		{
+			name: { cht: '陳冠蓉', eng: 'Rhonda Chen' },
+			html: '',
+		},
+	],
+	'12th ': [
+		{
+			name: { cht: '林奕瑄', eng: 'Grace Lin' },
+			html: '',
+		},
+		{
+			name: { cht: '鄭琮文', eng: 'Win-Win Cheng' },
+			html: '',
+		},
+		{
+			name: { cht: '賴彥廷', eng: 'Tim Lai' },
+			html: '',
+		},
+		{
+			name: { cht: '許茹涵', eng: 'Hannah Hsu' },
+			html: '',
+		},
+		{
+			name: { cht: '邱子瑜', eng: 'Darren Chiu' },
+			html: '',
+		},
+		{
+			name: { cht: '林芝羽', eng: 'Maggie Lin' },
+			html: '',
+		},
+		{
+			name: { cht: '吳若慈', eng: 'Rosy Wu' },
+			html: '',
+		},
+		{
+			name: { cht: '黃少甫', eng: 'Frank Huang' },
+			html: '',
+		},
+	],
+};
+export default {};

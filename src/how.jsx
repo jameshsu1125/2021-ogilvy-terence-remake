26a270d43c31fcd1a971f78e91a6e7363676e57a
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-10-28 11:54:22 +0800

    add gtag

--- src/how.jsx
+++ src/how.jsx
@@ -1,5 +1,8 @@
+import Gtag from 'lesca-gtag';
 import { render } from 'react-dom';
 import App from './How/main';
 import './Setting/global.less';
+
+Gtag.install(process.env.GA_ID);
 
 render(<App />, document.getElementById('app'));

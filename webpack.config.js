dd14fcbdb4155fe8ebcba71360dc7ea480c78224
jameshsu1125 <jameshsu1125@gmail.com>
2022-06-02 16:08:56 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = () => {
 		},
 		output: {
 			path: path.resolve(__dirname, 'dist'),
-			filename: `${Folder}/js/[name].min.js`,
+			filename: `${Folder}/js/[name].[hash].min.js`,
 			publicPath: NODE_ENV === 'production' ? './' : '/',
 		},
 		resolve: {

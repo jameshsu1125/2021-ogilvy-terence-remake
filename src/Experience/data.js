01c3af2e94a778a1771459a286cdf2f0a748f7e0
jameshsu1125 <jameshsu1125@gmail.com>
2022-09-16 14:09:10 +0800

    add 14th experience

--- src/Experience/data.js
+++ src/Experience/data.js
@@ -11,262 +11,98 @@ import { 李承恩, 林佑珊 } from './data/10th/data';
 import { 林堉涵, 劉亭妤, 方瑋婕, 盛以和, 方聖叡, 徐聖勛, 陳冠蓉 } from './data/11th/data';
 import { 林奕瑄, 鄭琮文, 賴彥廷, 許茹涵, 邱子瑜, 林芝羽, 吳若慈, 黃少甫 } from './data/12th/data';
 import { 鄭暐翰, 陳又慈, 李品萱, 劉昱伶, 洪名遠, 詹蕎 } from './data/13th/data';
+import { 王力萱, 林姿儀, 柯詠珩, 陳念微, 林妤晴 } from './data/14th/data';
 
 export const DefaultInternshipExperience = {
 	'2009 第一屆紅領帶': [
-		{
-			name: { cht: '詹曜維', eng: 'Tony Chan' },
-			html: 詹曜維,
-		},
-		{
-			name: { cht: '周于鈴', eng: 'Kilin Chou' },
-			html: 周于鈴,
-		},
-		{
-			name: { cht: '王俊凱', eng: 'Kai Wang' },
-			html: 王俊凱,
-		},
+		{ name: { cht: '詹曜維', eng: 'Tony Chan' }, html: 詹曜維 },
+		{ name: { cht: '周于鈴', eng: 'Kilin Chou' }, html: 周于鈴 },
+		{ name: { cht: '王俊凱', eng: 'Kai Wang' }, html: 王俊凱 },
 	],
 	'2010 第二屆紅領帶': [
-		{
-			name: { cht: '蔡承翰', eng: 'York Tsai' },
-			html: 蔡承翰,
-		},
-		{
-			name: { cht: '王彤', eng: 'Maxine Wang' },
-			html: 王彤,
-		},
-		{
-			name: { cht: '陳昱安', eng: 'Ellie Chen' },
-			html: 陳昱安,
-		},
-		{
-			name: { cht: '曾韻丞', eng: 'Andy Tseng' },
-			html: 曾韻丞,
-		},
-		{
-			name: { cht: '張競', eng: 'Jing Zhang' },
-			html: 張競,
-		},
-		{
-			name: { cht: '葉致綱', eng: 'Pablo Yeh' },
-			html: 葉致綱,
-		},
+		{ name: { cht: '蔡承翰', eng: 'York Tsai' }, html: 蔡承翰 },
+		{ name: { cht: '王彤', eng: 'Maxine Wang' }, html: 王彤 },
+		{ name: { cht: '陳昱安', eng: 'Ellie Chen' }, html: 陳昱安 },
+		{ name: { cht: '曾韻丞', eng: 'Andy Tseng' }, html: 曾韻丞 },
+		{ name: { cht: '張競', eng: 'Jing Zhang' }, html: 張競 },
+		{ name: { cht: '葉致綱', eng: 'Pablo Yeh' }, html: 葉致綱 },
 	],
 	'2011 第三屆紅領帶': [
-		{
-			name: { cht: '邱彥蓁', eng: 'Krista Chiu' },
-			html: 邱彥蓁,
-		},
-		{
-			name: { cht: '鄒玉珊', eng: 'Albee Tsao' },
-			html: 鄒玉珊,
-		},
-		{
-			name: { cht: '盧亭軒', eng: 'Iris Lu' },
-			html: 盧亭軒,
-		},
-		{
-			name: { cht: '蔡育旻', eng: 'Yumin Tsai' },
-			html: 蔡育旻,
-		},
+		{ name: { cht: '邱彥蓁', eng: 'Krista Chiu' }, html: 邱彥蓁 },
+		{ name: { cht: '鄒玉珊', eng: 'Albee Tsao' }, html: 鄒玉珊 },
+		{ name: { cht: '盧亭軒', eng: 'Iris Lu' }, html: 盧亭軒 },
+		{ name: { cht: '蔡育旻', eng: 'Yumin Tsai' }, html: 蔡育旻 },
 	],
 	'2012 第四屆紅領帶': [
-		{
-			name: { cht: '溫婷瑩', eng: 'Tiff Wen' },
-			html: 溫婷瑩,
-		},
-		{
-			name: { cht: '唐偉峻', eng: 'Willis Tang' },
-			html: 唐偉峻,
-		},
-		{
-			name: { cht: '曾繁皓', eng: 'Fanhou Zeng' },
-			html: 曾繁皓,
-		},
-		{
-			name: { cht: '范君達', eng: 'Jimmy Fan' },
-			html: 范君達,
-		},
-		{
-			name: { cht: '莊智淵', eng: 'Harper Zhuang' },
-			html: 莊智淵,
-		},
+		{ name: { cht: '溫婷瑩', eng: 'Tiff Wen' }, html: 溫婷瑩 },
+		{ name: { cht: '唐偉峻', eng: 'Willis Tang' }, html: 唐偉峻 },
+		{ name: { cht: '曾繁皓', eng: 'Fanhou Zeng' }, html: 曾繁皓 },
+		{ name: { cht: '范君達', eng: 'Jimmy Fan' }, html: 范君達 },
+		{ name: { cht: '莊智淵', eng: 'Harper Zhuang' }, html: 莊智淵 },
 	],
 	'2013 第五屆紅領帶': [
-		{
-			name: { cht: '牛嗣捷', eng: 'Lukas Niu' },
-			html: 牛嗣捷,
-		},
-		{
-			name: { cht: '汪世怡', eng: 'Sayee Wang' },
-			html: 汪世怡,
-		},
-		{
-			name: { cht: '涂子軒', eng: 'Dilly Tu' },
-			html: 涂子軒,
-		},
+		{ name: { cht: '牛嗣捷', eng: 'Lukas Niu' }, html: 牛嗣捷 },
+		{ name: { cht: '汪世怡', eng: 'Sayee Wang' }, html: 汪世怡 },
+		{ name: { cht: '涂子軒', eng: 'Dilly Tu' }, html: 涂子軒 },
 	],
-
 	'2014 第六屆紅領帶': [
-		{
-			name: { cht: '錢雅莛', eng: 'Ria Chien' },
-			html: 錢雅莛,
-		},
-		{
-			name: { cht: '江昀潔', eng: 'Yvonne Chiang' },
-			html: 江昀潔,
-		},
-		{
-			name: { cht: '白傑', eng: 'Jack Pai' },
-			html: 白傑,
-		},
+		{ name: { cht: '錢雅莛', eng: 'Ria Chien' }, html: 錢雅莛 },
+		{ name: { cht: '江昀潔', eng: 'Yvonne Chiang' }, html: 江昀潔 },
+		{ name: { cht: '白傑', eng: 'Jack Pai' }, html: 白傑 },
 	],
 	'2015 第七屆紅領帶': [
-		{
-			name: { cht: '何伯濤', eng: 'Casper Ho' },
-			html: 何伯濤,
-		},
-		{
-			name: { cht: '林叡志', eng: 'Danny Lin' },
-			html: 林叡志,
-		},
-		{
-			name: { cht: '施又寧', eng: 'Akimo Shih' },
-			html: 施又寧,
-		},
-		{
-			name: { cht: '林峻毅', eng: 'Alan Lin' },
-			html: 林峻毅,
-		},
+		{ name: { cht: '何伯濤', eng: 'Casper Ho' }, html: 何伯濤 },
+		{ name: { cht: '林叡志', eng: 'Danny Lin' }, html: 林叡志 },
+		{ name: { cht: '施又寧', eng: 'Akimo Shih' }, html: 施又寧 },
+		{ name: { cht: '林峻毅', eng: 'Alan Lin' }, html: 林峻毅 },
 	],
 	'2016 第八屆紅領帶': [
-		{
-			name: { cht: '林亞駿', eng: 'Will Lin' },
-			html: 林亞駿,
-		},
-		{
-			name: { cht: '蘇怡心', eng: 'Eunice Su' },
-			html: 蘇怡心,
-		},
+		{ name: { cht: '林亞駿', eng: 'Will Lin' }, html: 林亞駿 },
+		{ name: { cht: '蘇怡心', eng: 'Eunice Su' }, html: 蘇怡心 },
 	],
 	'2017 第九屆紅領帶': [
-		{
-			name: { cht: '王薇涵', eng: 'Lotus Wang' },
-			html: 王薇涵,
-		},
-		{
-			name: { cht: '嚴德鈞', eng: 'Tc Yen' },
-			html: 嚴德鈞,
-		},
-		{
-			name: { cht: '張容瑄', eng: 'Tina Chang' },
-			html: 張容瑄,
-		},
-		{
-			name: { cht: '彭亭翊', eng: 'Tina Peng' },
-			html: 彭亭翊,
-		},
+		{ name: { cht: '王薇涵', eng: 'Lotus Wang' }, html: 王薇涵 },
+		{ name: { cht: '嚴德鈞', eng: 'Tc Yen' }, html: 嚴德鈞 },
+		{ name: { cht: '張容瑄', eng: 'Tina Chang' }, html: 張容瑄 },
+		{ name: { cht: '彭亭翊', eng: 'Tina Peng' }, html: 彭亭翊 },
 	],
 	'2018 第十屆紅領帶': [
-		{
-			name: { cht: '李承恩', eng: 'Ben Lee' },
-			html: 李承恩,
-		},
-		{
-			name: { cht: '林佑珊', eng: 'Brook Lin' },
-			html: 林佑珊,
-		},
+		{ name: { cht: '李承恩', eng: 'Ben Lee' }, html: 李承恩 },
+		{ name: { cht: '林佑珊', eng: 'Brook Lin' }, html: 林佑珊 },
 	],
 	'2019 第十一屆紅領帶': [
-		{
-			name: { cht: '林堉涵', eng: 'Pi Lin' },
-			html: 林堉涵,
-		},
-		{
-			name: { cht: '劉亭妤', eng: 'Amy Liu' },
-			html: 劉亭妤,
-		},
-		{
-			name: { cht: '方瑋婕', eng: 'Erin Fang' },
-			html: 方瑋婕,
-		},
-		{
-			name: { cht: '盛以和', eng: 'Finnick Sheng' },
-			html: 盛以和,
-		},
-		{
-			name: { cht: '方聖叡', eng: 'FangFang Fang' },
-			html: 方聖叡,
-		},
-		{
-			name: { cht: '徐聖勛', eng: 'Andy Hsu' },
-			html: 徐聖勛,
-		},
-		{
-			name: { cht: '陳冠蓉', eng: 'Rhonda Chen' },
-			html: 陳冠蓉,
-		},
+		{ name: { cht: '林堉涵', eng: 'Pi Lin' }, html: 林堉涵 },
+		{ name: { cht: '劉亭妤', eng: 'Amy Liu' }, html: 劉亭妤 },
+		{ name: { cht: '方瑋婕', eng: 'Erin Fang' }, html: 方瑋婕 },
+		{ name: { cht: '盛以和', eng: 'Finnick Sheng' }, html: 盛以和 },
+		{ name: { cht: '方聖叡', eng: 'FangFang Fang' }, html: 方聖叡 },
+		{ name: { cht: '徐聖勛', eng: 'Andy Hsu' }, html: 徐聖勛 },
+		{ name: { cht: '陳冠蓉', eng: 'Rhonda Chen' }, html: 陳冠蓉 },
 	],
 	'2020 第十二屆紅領帶': [
-		{
-			name: { cht: '林奕瑄', eng: 'Grace Lin' },
-			html: 林奕瑄,
-		},
-		{
-			name: { cht: '鄭琮文', eng: 'Win-Win Cheng' },
-			html: 鄭琮文,
-		},
-		{
-			name: { cht: '賴彥廷', eng: 'Tim Lai' },
-			html: 賴彥廷,
-		},
-		{
-			name: { cht: '許茹涵', eng: 'Hannah Hsu' },
-			html: 許茹涵,
-		},
-		{
-			name: { cht: '邱子瑜', eng: 'Darren Chiu' },
-			html: 邱子瑜,
-		},
-		{
-			name: { cht: '林芝羽', eng: 'Maggie Lin' },
-			html: 林芝羽,
-		},
-		{
-			name: { cht: '吳若慈', eng: 'Rosy Wu' },
-			html: 吳若慈,
-		},
-		{
-			name: { cht: '黃少甫', eng: 'Frank Huang' },
-			html: 黃少甫,
-		},
+		{ name: { cht: '林奕瑄', eng: 'Grace Lin' }, html: 林奕瑄 },
+		{ name: { cht: '鄭琮文', eng: 'Win-Win Cheng' }, html: 鄭琮文 },
+		{ name: { cht: '賴彥廷', eng: 'Tim Lai' }, html: 賴彥廷 },
+		{ name: { cht: '許茹涵', eng: 'Hannah Hsu' }, html: 許茹涵 },
+		{ name: { cht: '邱子瑜', eng: 'Darren Chiu' }, html: 邱子瑜 },
+		{ name: { cht: '林芝羽', eng: 'Maggie Lin' }, html: 林芝羽 },
+		{ name: { cht: '吳若慈', eng: 'Rosy Wu' }, html: 吳若慈 },
+		{ name: { cht: '黃少甫', eng: 'Frank Huang' }, html: 黃少甫 },
 	],
 	'2021 第十三屆紅領帶': [
-		{
-			name: { cht: '鄭暐翰', eng: 'Wilson Cheng' },
-			html: 鄭暐翰,
-		},
-		{
-			name: { cht: '陳又慈', eng: 'Yuna Chen' },
-			html: 陳又慈,
-		},
-		{
-			name: { cht: '李品萱', eng: 'Purple Lee' },
-			html: 李品萱,
-		},
-		{
-			name: { cht: '劉昱伶', eng: 'Willow Liu' },
-			html: 劉昱伶,
-		},
-		{
-			name: { cht: '洪名遠', eng: 'Andre Hung' },
-			html: 洪名遠,
-		},
-		{
-			name: { cht: '詹蕎', eng: 'Joanne Chan' },
-			html: 詹蕎,
-		},
+		{ name: { cht: '鄭暐翰', eng: 'Wilson Cheng' }, html: 鄭暐翰 },
+		{ name: { cht: '陳又慈', eng: 'Yuna Chen' }, html: 陳又慈 },
+		{ name: { cht: '李品萱', eng: 'Purple Lee' }, html: 李品萱 },
+		{ name: { cht: '劉昱伶', eng: 'Willow Liu' }, html: 劉昱伶 },
+		{ name: { cht: '洪名遠', eng: 'Andre Hung' }, html: 洪名遠 },
+		{ name: { cht: '詹蕎', eng: 'Joanne Chan' }, html: 詹蕎 },
+	],
+	'2022 第十四屆紅領帶': [
+		{ name: { cht: '王力萱', eng: 'Abu Wang' }, html: 王力萱 },
+		{ name: { cht: '林姿儀', eng: 'Grace Lin' }, html: 林姿儀 },
+		{ name: { cht: '柯詠珩', eng: 'Street Ko' }, html: 柯詠珩 },
+		{ name: { cht: '陳念微', eng: 'WiWi Chen' }, html: 陳念微 },
+		{ name: { cht: '林妤晴', eng: 'Miffy Lin' }, html: 林妤晴 },
 	],
 };
 export default {};

c8e21f7505479fa042c5b61e03f2cf99dc0a6390
jameshsu1125 <jameshsu1125@gmail.com>
2023-02-14 12:03:25 +0800

    redirect root

--- src/how.js
+++ src/how.js
@@ -1,7 +1,13 @@
 import Gtag from 'lesca-gtag';
+import QueryString from 'lesca-url-parameters';
 import { createRoot } from 'react-dom/client';
 import App from './How/main';
 import './Setting/global.less';
+
+const { pathname } = window.location;
+if (pathname === 'terence') {
+	window.location.replace(`https://terence.ogilvy.com.tw/${QueryString.file()}`);
+}
 
 Gtag.install(process.env.GA_ID);
 createRoot(document.getElementById('app')).render(<App />);

93f2b3ad2d0f3549b808279194d3075b387bdaff
jameshsu1125 <jameshsu1125@gmail.com>
2023-03-24 20:10:55 +0800

    Update main.js

--- src/Components/Alert/main.js
+++ src/Components/Alert/main.js
@@ -26,26 +26,27 @@ const Alert = (props) => {
 			<div ref={containerRef} className='alert-container'>
 				<div className='my-5 space-y-4'>
 					<div className='title'>
-						<h1>【第十五屆 奧美林宗緯紅領帶計劃】報名開始</h1>
+						<h1>【第十五屆 奧美林宗緯紅領帶計劃】筆試公告</h1>
 					</div>
 					<p>
-						詳細申請流程請參考
-						<a className='mx-1 hover:underline' href='./how.html' target='_self'>
-							[如何申請]
-						</a>
+						線上筆試作答時間
+						<br />
+						台灣時間 3月29日 18:00 – 21:00
+						<br />
+						線上筆試連結將於上述時間寄送至報名時提供之電子信箱
+						<br />
+						請自行確認電子信箱運作正常
 					</p>
-				</div>
-				<hr />
-				<div className='my-5 space-y-4'>
-					<button
-						className=''
-						onClick={() => {
-							window.open('https://forms.gle/8qQJXZ7VXJgFLcSt7');
-						}}
-						type='button'
-					>
-						*即刻報名*
-					</button>
+					<hr />
+					<p>
+						入選筆試者將同步發送筆試通知信，3/28日前仍未收到通知信或有其他問題
+						<br />
+						請來信
+						<a className='mx-1 hover:underline' href='mailto:amyty.chen@ogilvy.com'>
+							amyty.chen@ogilvy.com
+						</a>
+						詢問
+					</p>
 				</div>
 				<div className='alert-close' onClick={close} onKeyPress={close} role='none' />
 			</div>
